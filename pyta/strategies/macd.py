83bc681dd6dfda1e49cdee4eade1820d4fdcfe48
Aeol Kong <kongscn@gmail.com>
2013-06-28 22:09:59 +0800

    cleanup

--- pyta/strategies/macd.py
+++ pyta/strategies/macd.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# todo: the plot func.
 """
 This should only be used as a demonstration, as prices are not precise,
 nor are properly adjusted for splits and dividends.
@@ -7,25 +6,26 @@ nor are properly adjusted for splits and dividends.
 """
 __author__ = 'kongs'
 
+import logging
+from os.path import join
+
 import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.dates as mdates
-import matplotlib.ticker as mticker
-import matplotlib.pyplot as plt
-import matplotlib.font_manager as font_manager
-
+# import matplotlib
+# import matplotlib.dates as mdates
+# import matplotlib.ticker as mticker
+# import matplotlib.pyplot as plt
+# import matplotlib.font_manager as font_manager
 
 import utils.log
-import logging
-
-# from utils.config import CONFIG
-import os.path as path
-
-
+from utils.config import CONFIG
 from strategies.strategy import Strategy
 
+logger = logging.getLogger(__name__)
+
+
 class MACD(Strategy):
+
     """You have the option to change the slow, fast and signal periods used to calculate the MACD above.
     The default slow, fast and signal periods are set to 26, 12 and 9, respectively.
     Moving Average Convergence Divergence (MACD) is a trend-following momentum indicator
@@ -51,20 +51,17 @@ class MACD(Strategy):
      - "Technical Analysis from A to Z" by Stephen Aechlis
 
     """
-    CONFIGKEYS = 'nfast nslow nmacd algo algo_ema weight products column'.split()
-    COLORS = '''Aqua Blue BlueViolet Brown Chartreuse Chocolate DarkCyan DarkMagenta
-            DarkOrchid DarkViolet LawnGreen SteelBlue'''.split()
 
     def __init__(self, fast=12, slow=26, cdma=9,
                  column='Close', verbose=False):
-        self.logger = logging.getLogger(__name__)
+
         self.nfast = fast
         self.nslow = slow
         self.nmacd = cdma
         self.column = column
         self.verbose = verbose
-        self._product=None
-        self.prices=None
+        self._product = None
+        self.prices = None
 
         Strategy.__init__(self)
 
@@ -75,243 +72,196 @@ class MACD(Strategy):
     @product.setter
     def product(self, p):
         self._product = p
-        self.prices=p[self.column]
+        self.prices = p[self.column].values
+        self.date_idx = p.index
 
     def analyze(self):
-
         init_asset = 1000
-        # if self.prices is None:
-        #     prices = self.product[self.column]
-        # else:
         prices = self.prices
+        recs = {}
 
-        recs=dict()
-
-
-        # ma_fast = ma.MA(prices, self.nfast, algo=self.algo, weights=self.weight)()
         recs['ma_fast'] = pd.ewma(prices, span=self.nfast)
-        # recs['ma_fast'] = pd.rolling_mean(prices, self.nfast)
         recs['ma_slow'] = pd.ewma(prices, span=self.nslow)
-        # recs['ma_slow'] = pd.rolling_mean(prices, self.nslow)
         recs['macd'] = recs['ma_fast'] - recs['ma_slow']
         recs['signal'] = pd.ewma(recs['macd'], span=self.nmacd)
-        # recs['signal'] = pd.rolling_mean(recs['macd'], self.nmacd)
         recs['hist'] = recs['macd'] - recs['signal']
+        recs['price'] = prices
 
-
-        recs['price']=prices
-        # recs=pd.DataFrame(recs)
-        # self.recs.to_csv('before_jop.csv')
-        # recs = self.recs.dropna()
-        # self.recs = fromarrays(self.result, names=['date', 'mafast', 'maslow', 'macd', 'signal', 'hist'])
-
-
-        # first = self.nfast
-        # init_asset = 1000
-        long = recs['hist'].values > 0
-
-        flags = np.logical_xor(long[1:], long[:-1])
-
+        long = recs['hist'] > 0
+        flags = long[1:] ^ long[:-1]
         if np.bincount(flags)[1] % 2 == 1:
             flags[-1] = not flags[-1]
 
-        # recs['trade_flag']=flags
-        # recs.at[recs.index[0], 'trade_flag']=False
-
-        # change last flag, if long position, sell it.
-        # if recs['trade_flag'].value_counts().loc[True] % 2 == 1:
-        # flags=recs['trade_flag'].astype('bool')
-        #
-        #     recs.at[recs.index[-1], 'trade_flag'] = \
-        #             not recs.at[recs.index[-1], 'trade_flag']
-
-
-        # trade_date = p.date[first:][trades]
-        # assert len(recs['price'][1:]) == len(flags)
-        trade_price = recs['price'][1:].values[flags]
-        # trade_price.name='price'
-
-        # trade_count = len(trade_price)
-        # buys stays at odd position and sells even
-        #get buy prices
-        if len(trade_price) == 0:
-            self.trades = None
-            self.revenue = init_asset
-            return
-
-
-        self.trade_count=len(trade_price)
-
-        # self.logger.info('trade prices: %s'%str(trade_price))
-
-        trade_price[::2]=1/trade_price[::2]
+        trade_price = prices[1:][flags]
+        trade_price[::2] = 1 / trade_price[::2]
         self.revenue = init_asset * trade_price.prod()
+        self.trade_count = len(trade_price)
 
-        # trades = pd.DataFrame(trade_price)
-        #
-        # trades['long']=True
-        # trades['long'][1::2]=False
-        #
-        # buy_prices = trades['price'][trades['long']]
-        # sell_prices = trades['price'][~trades['long']]
-        #
-        # if len(sell_prices)>0:
-        #     sell_price_prod = sell_prices.prod()
-        # else:
-        #     sell_price_prod = prices[-1]
-        # self.revenue = init_asset / buy_prices.prod() * sell_price_prod
-        # self.trades = trades
-        #
-        # self.trade_count=len(trades)
-        #
         if self.verbose:
-            recs.to_csv('all_recs.csv')
-            trade_price.to_csv('trade_price.csv')
+            analysis_file = join(CONFIG['out_p'], 'analysis.csv')
+            trans_file = join(CONFIG['out_p'], 'trans.csv')
+
+            recs = pd.DataFrame(recs, index=self.date_idx)
+            recs['long'] = long
+            recs['trade_flag'] = np.insert(flags, 0, False)
+            recs.to_csv(analysis_file)
+
+            trade_price = pd.Series(
+                prices[1:][flags], index=self.date_idx[1:][flags])
+            trade_price.name = 'price'
+            trades = pd.DataFrame(trade_price)
+            trades['action'] = 'buy'
+            trades['action'][1::2] = 'sell'
+            trades.to_csv(trans_file)
 
     def summary(self):
-        s=('%-4s Hist Data: %s from %s to %s;\n'
-        'Trade stat: %s trades, total revenue %s')
+        s = ('%-4s Hist Data: %s from %s to %s;\n'
+             'Trade stat: %s trades, total revenue %s')
 
-        s = s%(self.product.code, len(self.product), self.product.index[0], self.product.index[-1],
-               self.trade_count, self.revenue)
-        self.desc=s
+        s = s % (self.product.code, len(self.product),
+                 self.product.index[0], self.product.index[-1],
+                 self.trade_count, self.revenue)
+        self.desc = s
 
-        self.logger.info(self.desc)
+        logger.info(self.desc)
 
     def plot(self):
-        colors = self.__class__.COLORS
-        plt.rc('axes', grid=True)
-        plt.rc('grid', color='0.75', linestyle='-', linewidth=0.5)
+        raise NotImplementedError
 
-        text_size = 9
-        left, width = 0.1, 0.8
-        rect_price = [left, 0.5, width, 0.4]  #left, bottom, width, height
-        rect_volume = [left, 0.3, width, 0.2]
-        rect_macd = [left, 0.1, width, 0.2]
-
-        fig = plt.figure(facecolor='white')
-        axes_color = '#f6f6f6'  # the axes background color
-
-        ax_price = fig.add_axes(rect_price, axisbg=axes_color)
-        ax_volume = fig.add_axes(rect_volume, axisbg=axes_color, sharex=ax_price)
-        ax_macd = fig.add_axes(rect_macd, axisbg=axes_color, sharex=ax_price)
-
-        # formatter for prices
-        def to_percent(y, position):
-            # Ignore the passed in position. This has the effect of scaling the default
-            # tick locations.
-            if len(self.products) == 1:
-                return y
-            s = str(100 * y)
-
-            # The percent symbol needs escaping in latex
-            if matplotlib.rcParams['text.usetex'] == True:
-                return s + r'$\%$'
-            else:
-                return s + '%'
-
-        formatter = mticker.FuncFormatter(to_percent)
-
-        ### plot the price and volume data: Open, High, Low, Close
-        # use open, high, low, close. adjust in product class before strategy back testing.
-        # currently it's only a vertical line that indicates the open and close price
-        # todo: add ohlc and candle chart type.
-        # dx = product.adj_close - product.close
-
-        # if there're more than one product, convert prices to percent
-        if len(self.products) > 1:
-            for p in self.products:
-                p.convert_to_percent()
-
-
-        # plot the main product.
-        product = self.products[0]
-        ax_price.set_title('%s' % product.code)
-        open_p = product.open
-        close_p = product.close
-
-        deltas = close_p - open_p
-        up = deltas > 0
-        ax_price.vlines(product.date[up], open_p[up], close_p[up], color='green', label='_nolegend_')
-        ax_price.vlines(product.date[~up], open_p[~up], close_p[~up], color='red', label='_nolegend_')
-
-        # plot MA only if no products to compare
-        if len(self.products) == 1:
-            ax_price.plot(self.result[0], self.result[1], color='blue', lw=2, label='MA (%d)' % self.nfast)
-            ax_price.plot(self.result[0], self.result[2], color='red', lw=2, label='MA (%d)' % self.nslow)
-
-        last = product[-1]
-        s = '%s O:%1.2f H:%1.2f L:%1.2f C:%1.2f, V:%1.1fM Chg:%+1.2f' % (
-            last.date.strftime('%d-%b-%Y'),
-            last.open, last.high,
-            last.low, last.close,
-            last.volume * 1e-6,
-            last.close - last.open )
-        t4 = ax_price.text(0.3, 0.9, s, transform=ax_price.transAxes, fontsize=text_size)
-
-        # print the compare products
-        for p in self.products[1:]:
-            ax_price.plot(p.date, p.close, color=colors.pop(), lw=1, label=p.code)
-
-        #ax_price.set_yscale('log')
-        ax_price.yaxis.set_major_formatter(formatter)
-
-
-        # legend
-        props = font_manager.FontProperties(size=10)
-        leg = ax_price.legend(loc='center left', shadow=True, fancybox=True, prop=props)
-        leg.get_frame().set_alpha(0.5)
-
-        # volume = (product.close * product.volume) / 1e6  # dollar volume in millions
-        # vmax = volume.max()
-        volume = product.volume
-
-        fillcolor = 'darkgoldenrod'
-        poly = ax_volume.fill_between(product.date, volume, 0, label='Volume', facecolor=fillcolor, edgecolor=fillcolor)
-        ax_volume.set_ylim(0, volume.max())
-        ax_volume.set_yticks([])
-
-        ### compute the MACD indicator
-        fillcolor = 'darkslategrey'
-
-        macd = self.result[3]
-        ema = self.result[4]
-        ax_macd.plot(product.date, macd, color='black', lw=2)
-        ax_macd.plot(product.date, ema, color='blue', lw=1)
-        ax_macd.fill_between(product.date, macd - ema, 0, alpha=0.5, facecolor=fillcolor, edgecolor=fillcolor)
-
-        ax_macd.text(0.025, 0.95, 'MACD (%d, %d, %d)' % (self.nfast, self.nslow, self.nmacd), va='top',
-            transform=ax_macd.transAxes, fontsize=text_size)
-
-        for ax in ax_price, ax_volume, ax_macd:
-            if ax != ax_macd:
-                for label in ax.get_xticklabels():
-                    label.set_visible(False)
-            else:
-                for label in ax.get_xticklabels():
-                    label.set_rotation(30)
-                    label.set_horizontalalignment('right')
-
-            ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
-
-        class MyLocator(mticker.MaxNLocator):
-            def __init__(self, *args, **kwargs):
-                mticker.MaxNLocator.__init__(self, *args, **kwargs)
-
-            def __call__(self, *args, **kwargs):
-                return mticker.MaxNLocator.__call__(self, *args, **kwargs)
-
-        # at most 5 ticks, pruning the upper and lower so they don't overlap
-        # with other ticks
-        #ax_macd.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
-        #ax3.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
-
-        ax_price.yaxis.set_major_locator(MyLocator(5, prune='both'))
-        ax_macd.yaxis.set_major_locator(MyLocator(5, prune='both'))
-
-        # fig.savefig(path.join(CONFIG['temp_p'], 'macd.pdf'))
-        plt.show(block=True)
-
+    # def plot(self):
+    #     colors = self.__class__.COLORS
+    #     plt.rc('axes', grid=True)
+    #     plt.rc('grid', color='0.75', linestyle='-', linewidth=0.5)
+    #
+    #     text_size = 9
+    #     left, width = 0.1, 0.8
+    # rect_price = [left, 0.5, width, 0.4]  #left, bottom, width, height
+    #     rect_volume = [left, 0.3, width, 0.2]
+    #     rect_macd = [left, 0.1, width, 0.2]
+    #
+    #     fig = plt.figure(facecolor='white')
+    # axes_color = '#f6f6f6'  # the axes background color
+    #
+    #     ax_price = fig.add_axes(rect_price, axisbg=axes_color)
+    #     ax_volume = fig.add_axes(rect_volume, axisbg=axes_color, sharex=ax_price)
+    #     ax_macd = fig.add_axes(rect_macd, axisbg=axes_color, sharex=ax_price)
+    #
+    # formatter for prices
+    #     def to_percent(y, position):
+    # Ignore the passed in position. This has the effect of scaling the default
+    # tick locations.
+    #         if len(self.products) == 1:
+    #             return y
+    #         s = str(100 * y)
+    #
+    # The percent symbol needs escaping in latex
+    #         if matplotlib.rcParams['text.usetex'] == True:
+    #             return s + r'$\%$'
+    #         else:
+    #             return s + '%'
+    #
+    #     formatter = mticker.FuncFormatter(to_percent)
+    #
+    # plot the price and volume data: Open, High, Low, Close
+    # use open, high, low, close. adjust in product class before strategy back testing.
+    # currently it's only a vertical line that indicates the open and close price
+    # todo: add ohlc and candle chart type.
+    # dx = product.adj_close - product.close
+    #
+    # if there're more than one product, convert prices to percent
+    #     if len(self.products) > 1:
+    #         for p in self.products:
+    #             p.convert_to_percent()
+    #
+    #
+    # plot the main product.
+    #     product = self.products[0]
+    #     ax_price.set_title('%s' % product.code)
+    #     open_p = product.open
+    #     close_p = product.close
+    #
+    #     deltas = close_p - open_p
+    #     up = deltas > 0
+    #     ax_price.vlines(product.date[up], open_p[up], close_p[up], color='green', label='_nolegend_')
+    #     ax_price.vlines(product.date[~up], open_p[~up], close_p[~up], color='red', label='_nolegend_')
+    #
+    # plot MA only if no products to compare
+    #     if len(self.products) == 1:
+    #         ax_price.plot(self.result[0], self.result[1], color='blue', lw=2, label='MA (%d)' % self.nfast)
+    #         ax_price.plot(self.result[0], self.result[2], color='red', lw=2, label='MA (%d)' % self.nslow)
+    #
+    #     last = product[-1]
+    #     s = '%s O:%1.2f H:%1.2f L:%1.2f C:%1.2f, V:%1.1fM Chg:%+1.2f' % (
+    #         last.date.strftime('%d-%b-%Y'),
+    #         last.open, last.high,
+    #         last.low, last.close,
+    #         last.volume * 1e-6,
+    #         last.close - last.open )
+    #     t4 = ax_price.text(0.3, 0.9, s, transform=ax_price.transAxes, fontsize=text_size)
+    #
+    # print the compare products
+    #     for p in self.products[1:]:
+    #         ax_price.plot(p.date, p.close, color=colors.pop(), lw=1, label=p.code)
+    #
+    # ax_price.set_yscale('log')
+    #     ax_price.yaxis.set_major_formatter(formatter)
+    #
+    #
+    # legend
+    #     props = font_manager.FontProperties(size=10)
+    #     leg = ax_price.legend(loc='center left', shadow=True, fancybox=True, prop=props)
+    #     leg.get_frame().set_alpha(0.5)
+    #
+    # volume = (product.close * product.volume) / 1e6  # dollar volume in millions
+    # vmax = volume.max()
+    #     volume = product.volume
+    #
+    #     fillcolor = 'darkgoldenrod'
+    #     poly = ax_volume.fill_between(product.date, volume, 0, label='Volume', facecolor=fillcolor,
+    # edgecolor=fillcolor)
+    #     ax_volume.set_ylim(0, volume.max())
+    #     ax_volume.set_yticks([])
+    #
+    # compute the MACD indicator
+    #     fillcolor = 'darkslategrey'
+    #
+    #     macd = self.result[3]
+    #     ema = self.result[4]
+    #     ax_macd.plot(product.date, macd, color='black', lw=2)
+    #     ax_macd.plot(product.date, ema, color='blue', lw=1)
+    #     ax_macd.fill_between(product.date, macd - ema, 0, alpha=0.5, facecolor=fillcolor, edgecolor=fillcolor)
+    #
+    #     ax_macd.text(0.025, 0.95, 'MACD (%d, %d, %d)' % (self.nfast, self.nslow, self.nmacd), va='top',
+    #                  transform=ax_macd.transAxes, fontsize=text_size)
+    #
+    #     for ax in ax_price, ax_volume, ax_macd:
+    #         if ax != ax_macd:
+    #             for label in ax.get_xticklabels():
+    #                 label.set_visible(False)
+    #         else:
+    #             for label in ax.get_xticklabels():
+    #                 label.set_rotation(30)
+    #                 label.set_horizontalalignment('right')
+    #
+    #         ax.fmt_xdata = mdates.DateFormatter('%Y-%m-%d')
+    #
+    #     class MyLocator(mticker.MaxNLocator):
+    #         def __init__(self, *args, **kwargs):
+    #             mticker.MaxNLocator.__init__(self, *args, **kwargs)
+    #
+    #         def __call__(self, *args, **kwargs):
+    #             return mticker.MaxNLocator.__call__(self, *args, **kwargs)
+    #
+    # at most 5 ticks, pruning the upper and lower so they don't overlap
+    # with other ticks
+    # ax_macd.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
+    # ax3.yaxis.set_major_locator(mticker.MaxNLocator(5, prune='both'))
+    #
+    #     ax_price.yaxis.set_major_locator(MyLocator(5, prune='both'))
+    #     ax_macd.yaxis.set_major_locator(MyLocator(5, prune='both'))
+    #
+    # fig.savefig(path.join(CONFIG['temp_p'], 'macd.pdf'))
+    #     plt.show(block=True)
 
     def apply(self):
         self.analyze()
@@ -323,32 +273,19 @@ class MACD(Strategy):
 
 
 if __name__ == '__main__':
-    # from test.getsample import get_product_ex
-    # from test.getsample import get_product_exs
+    from pandas.io.data import DataReader
 
     date_from = '2003-01-01'
     date_to = '2013-01-01'
 
-    from pandas.io.data import DataReader, get_components_yahoo
+    model = MACD(12, 26, 9, verbose=True)
 
-
-    # # products = get_product_exs()
-    # print(product.describe())
-    # # print(products[-1].summary())
-
-
-    mymacd = MACD(12, 26, 9, verbose=False)
-
-    code='GOOG'
+    code = 'GOOG'
     product = DataReader(code, 'yahoo', start=date_from, end=date_to)
-    product.code=code
-    mymacd.product=product
-    mymacd.analyze()
-    mymacd.summary()
+    product.code = code
+    model.product = product
+    model.analyze()
+    model.summary()
 
-
-    # products=get_components_yahoo('^DJI')
-    # for code in products.index:
-    #     product=DataReader(code, 'yahoo')
-    #     product.code=code
-    #     mymacd(product)
+    # From 2003-01-01 to 2013-01-01, MACD(12, 26, 9)
+    # Trade stat: 154 trades, total revenue 6904.1657838
--- pyta/strategies/macdopt.py
+++ pyta/strategies/macdopt.py
@@ -6,80 +6,71 @@
 __author__ = 'kongs'
 # TODO: add method to store result.
 
-import os.path as path
+from os.path import join
 import logging
+import time
 
 from numpy.core.records import fromrecords
 import pandas as pd
 from pandas.io.data import DataReader, get_components_yahoo
 
-import utils.log
 from utils.config import CONFIG
 from strategies.macd import MACD
 from utils.common import rec2csv
 
 
-
 logger = logging.getLogger(__name__)
 
+
 def opt(products):
-    import time
-    from datetime import datetime as dt
-    time_start=time.time()
-    # print('Start at %s'%time_start)
-    recs=[]
-    calculations=0
+    time_start = time.time()
+    recs = []
+    calculations = 0
     model = MACD()
 
     for product in products:
-        model.product=product
+        model.product = product
         cals, rec = opt_single(model)
         recs += rec
         calculations += cals
 
-    recs = fromrecords(recs, names=['code', 'revenue', 'nfast', 'nslow', 'nmacd'])
-    rec2csv(recs, path.join(CONFIG['out_p'],'opt.csv'), mode='a', with_header=True)
+    time_end = time.time()
+    recs = fromrecords(recs, names=['code', 'revenue',
+                                    'nfast', 'nslow', 'nmacd'])
+    rec2csv(recs, join(CONFIG['out_p'], 'opt.csv'))
 
-    time_end=time.time()
-    pst=time_end - time_start
-    # print('End at %s' % time_end)
-    print('Total %s para combinations computed, roughly %.3f seconds, %.3f ms per calc.' % (
-                calculations, pst, pst/calculations*1000))
+    pst = time_end - time_start
+
+    print(('Total %s para combinations computed,'
+           'roughly %.3f seconds, %.3f ms per calc.') % (
+              calculations, pst, pst / calculations * 1000))
 
 
 def opt_single(model):
-    # if len(p) < 200:
-    #     logger.warning('CODE %s has only %d records, not enough.', p.code, len(p))
-    #     return
     cals = 0
-
     recs = []
 
     for nfast in range(6, 24):
-        #logger.info('Start CODE=%s, nfast=%d', model.products[0].code, nfast)
-        for nslow in range(nfast+5, 31):
+        for nslow in range(nfast + 5, 31):
             for nmacd in range(6, 15):
                 cals += 1
-                model.nfast=nfast
-                model.nslow=nslow
-                model.nmacd=nmacd
+                model.nfast = nfast
+                model.nslow = nslow
+                model.nmacd = nmacd
                 model.analyze()
-                recs.append((model.product.code, model.revenue, nfast, nslow, nmacd))
+                recs.append((model.product.code, model.revenue,
+                             nfast, nslow, nmacd))
 
     return cals, recs
 
 
 def main():
-    period = 'd'
-    date_from = '2003-01-01'
-    date_to = '2013-01-01'
-
-#    db = psql.get_db()
-#    product_codes = db.prepare('''SELECT p.code AS code, p.id as id
-#        FROM products p LEFT JOIN companies c ON p.company_id = c.id
-#        WHERE c.sector IS NOT NULL
-#        and p.id < 9354
-#        ORDER BY p.id''')()
+    #    db = psql.get_db()
+    #    product_codes = db.prepare('''SELECT p.code AS code, p.id as id
+    #        FROM products p LEFT JOIN companies c ON p.company_id = c.id
+    #        WHERE c.sector IS NOT NULL
+    #        and p.id < 9354
+    #        ORDER BY p.id''')()
 
     product_codes = get_components_yahoo('^DJI').index
     cur = 0
@@ -88,8 +79,7 @@ def main():
 
     for code in product_codes:
         cur += 1
-        logger.info('(%d/%d) %s started.',cur, all, code)
-
+        logger.info('(%d/%d) %s started.', cur, all, code)
 
         product = DataReader(code, 'yahoo', start=date_from, end=date_to)
 
@@ -97,26 +87,34 @@ def main():
 
         opt(product)
 
+
 def demo():
-    code='AAPL'
-    date_from = '2003-01-01'
-    date_to = '2013-01-01'
+
     product = DataReader(code, 'yahoo', start=date_from, end=date_to)
     product.code = code
     opt([product])
 
+
 def democsv():
     from utils.config import CONFIG
     from os.path import join
-    histf=join(CONFIG['temp_p'], 'AAPL.csv')
+
+    histf = join(CONFIG['temp_p'], 'AAPL.csv')
     product = pd.read_csv(histf, index_col=0)
-    product.code='aapl'
+    product.code = 'aapl'
     opt([product])
 
 
 if __name__ == '__main__':
+
+    period = 'd'
+    code = 'AAPL'
+    date_from = '2003-01-01'
+    date_to = '2013-01-01'
+
     # main()
 
     from timeit import Timer
+
     t = Timer("demo()", "from __main__ import demo")
     print(t.repeat(repeat=1, number=1))
